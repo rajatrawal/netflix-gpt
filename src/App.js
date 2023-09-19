70188ee0311b3c7f39ed290160ea7fa386234f64
Rajat <rwlrajat@gmail.com>
2023-09-19 13:31:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='text-center text-3xl mt-4 text-red-600 font-bold'>Netfilx Gpt</h1>
+    </div>
+  );
+}
+
+export default App;
